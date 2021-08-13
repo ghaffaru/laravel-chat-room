b152066603c84887d8a4643fe21550c71c5e5da2
Ghaff <mudashiruagm@gmail.com>
2021-08-13 07:30:51 +0000

    set up laravel breeze and pusher

---
+++ resources/js/components/MessageArea.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="app" >
+        <div>
+
+            <li class="list-group-item" :class="className"><slot></slot></li>
+            <small>You</small>
+            <b-badge variant="primary">Yh</b-badge>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'message-area',
+    props: ['color'],
+    data() {
+        return {
+
+        }
+    },
+    computed: {
+        className() {
+            return 'list-group-item-' + this.color;
+        },
+        badgeClass() {
+            return  this.color;
+        }
+    },
+    mounted() {
+
+    }
+}
+</script>
---
+++ resources/views/chat.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Chat Room app with laravel</title>
+
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+    <style>
+        .list-group {
+            overflow-y: auto;
+            height: 200px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container" id="app">
+        <div class="row">
+            <div class="offset-4 col-4">
+                <li class="list-group-item active" >Chat Room</li>
+                <ul class="list-group" v-chat-scroll>
+
+                    <message-area v-for="message in messages" :key="message" color="success">@{{ message }}</message-area>
+
+                </ul>
+                <input type="text" class="form-control" placeholder="Type your message here ..." v-model="message" @keyup.enter="send"/>
+            </div>
+
+        </div>
+    </div>
+<script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
